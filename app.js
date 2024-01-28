f35efabe61acf5b96ffda1ea7f0be794df519bbc
HAMBUK <108109292+HAMBUK@users.noreply.github.com>
2024-01-28 15:56:48 +0900

    #4.4 Getting Username

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
     event.preventDefault();
     const username = loginInput.value;
-    console.log(username);
-}
-
-function handleLinkClick(event){
-    event.preventDefault();
-    console.log(event);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 
 loginForm.addEventListener("submit", onLoginSubmit);
-
-link.addEventListener("click", handleLinkClick)
