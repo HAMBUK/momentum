91863d9a7d3eb4a8243330dfb93a0b73028c3c39
HAMBUK <108109292+HAMBUK@users.noreply.github.com>
2024-01-28 20:29:34 +0900

    #7.6 Deleting To Dos part One

--- js/todo.js
+++ js/todo.js
@@ -12,13 +12,15 @@ function saveToDos(){
 
 function deleteToDo(event){
     const li = event.target.parentElement;
+    console.log(li.id);
     li.remove();
 }
 
 function paintToDo(newTodo){
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "❌"
     button.addEventListener("click", deleteToDo)
@@ -31,8 +33,12 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    }
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
@@ -40,7 +46,7 @@ toDoForm.addEventListener("submit", handleToDoSubmit);
 
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
-if(saveToDos !== null ){
+if(savedToDos !== null ){
     const parsedTodos = JSON.parse(savedToDos);
     toDos = parsedTodos;
     parsedTodos.forEach((item) => paintToDo(item));
